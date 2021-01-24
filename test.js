e2aa8cc5fb879ece3d13c3d7a242d57691cedd69
Giansingh4710 <giansingh4710@gmail.com>
2021-01-24 14:52:16 -0500

    switch cases

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-let name=prompt("What is your Name?")
-document.getElementById("name").innerHTML="Hello "+name;
+let name=prompt("What is your Name? ")
+//document.getElementById("name").innerHTML="Hello "+name;
+switch(name){
+    case "Gian":
+        console.log("thats meee");
+        break;
+    default:
+        console.log("not mee");
+        break;
+}
 
 
