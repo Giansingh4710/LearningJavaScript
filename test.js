2d6ed5aaaafe299c790c67843ada294d8c78a8dd
Giansingh4710 <giansingh4710@gmail.com>
2021-01-24 14:07:41 -0500

    prompts

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
-
-document.getElementById("count").onclick= function(){
-    let a=document.getElementById("num").textContent;
-    a++;
-    document.getElementById("num").innerHTML=a;
-    //document.getElementById("count").style.display= "none";
-}
+let a=prompt("write something");
+alert(a);
 
 
