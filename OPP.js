e1d6a0d04244f645612445aaa32af58ac36b6710
Gian <giansingh4710@gmail.com>
2021-11-12 16:42:15 -0500

    rand

--- OPP.js
+++ OPP.js
@@ -1,21 +1,28 @@
-let uni = {
-  name: function (name) {
-    this.name = name;
-  },
-  students: [],
-  stud: function (n) {
-    this.students.push(n);
-  },
-  allStudents: function () {
-    for (let i = 0; i < this.students.length; i++) {
-      console.log(this.students[i]);
-    }
-  },
-};
+function uni(theUniName) {
+  return {
+    name: theUniName,
+    students: [],
+    stud: function (n) {
+      this.students.push(n);
+    },
+    allStudents: function () {
+      console.log("Students from " + this.name + ":");
+      for (let i = 0; i < this.students.length; i++) {
+        console.log(this.students[i]);
+      }
+      console.log();
+    },
+  };
+}
 
-let a = uni;
-a.name("NJIT");
+let a = uni("NJIT");
 a.stud("Bob");
 a.stud("Jack");
-a.stud("Bob");
-//a.allStudents()
+a.stud("joe");
+a.allStudents();
+
+let b = uni("RU");
+b.stud("Jone");
+b.stud("Rat");
+b.stud("Row");
+b.allStudents();
